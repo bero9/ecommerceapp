7ffebfaa16df53ffdeaa03900c23d14ba3c47961
bero9 <berohamood545@gmail.com>
2024-01-02 15:54:02 +0300

    nothing

--- lib/common/widgets/text/Section_Heading.dart
+++ lib/common/widgets/text/Section_Heading.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 
-
 class TSeactionHeading extends StatelessWidget {
   const TSeactionHeading({
-    super.key, this.textColor,  this.showActionButtom=true, required this.title, this.buttomTitle='view all', this.onPressd,
+    super.key,
+    this.textColor,
+    this.showActionButtom = true,
+    required this.title,
+    this.buttomTitle = 'view all',
+    this.onPressd,
   });
+
   final Color? textColor;
   final bool showActionButtom;
-  final String title ,buttomTitle;
+  final String title, buttomTitle;
   final void Function()? onPressd;
+
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween
+
+        ,children: [
       Text(
         title,
-        style: Theme.of(context).textTheme.headlineSmall!.apply(color: textColor),
+        style:
+            Theme.of(context).textTheme.headlineSmall!.apply(color: textColor),
         maxLines: 1,
         overflow: TextOverflow.ellipsis,
       ),
-      if(showActionButtom)TextButton(
-        onPressed: onPressd,
-        child: Text(buttomTitle),
-      ),
+      if (showActionButtom)
+        TextButton(
+          onPressed: onPressd,
+          child: Text(buttomTitle),
+        ),
     ]);
   }
 }
